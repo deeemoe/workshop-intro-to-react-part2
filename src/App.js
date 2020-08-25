d51f785622cff136eee39aadaa38ac066df27e33
Dustin Möller <dustin.moeller@di-unternehmer.com>
2020-08-25 17:14:29 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.scss";
+
+const App = () => {
+	return <div className="main">This is my main react </div>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+	entry: "./src/index.js",
+	module: {
+		rules: [
+			{ test: /\.(js)$/, use: "babel-loader" },
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					// Creates `style` nodes from JS strings
+					process.env.NODE_ENV !== "production"
+						? "style-loader"
+						: MiniCssExtractPlugin.loader,
+					// Translates CSS into CommonJS
+					"css-loader",
+					// Compiles Sass to CSS
+					{
+						loader: "sass-loader",
+						options: {
+							sourceMap: true,
+							sassOptions: {
+								outputStyle: "compressed",
+							},
+						},
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			template: "./src/index.html",
+		}),
+		new MiniCssExtractPlugin({
+			// Options similar to the same options in webpackOptions.output
+			// both options are optional
+			filename: "[name].css",
+			chunkFilename: "[id].css",
+		}),
+	],
+	output: {
+		path: path.resolve(__dirname, "/public"),
+		filename: "bundle.js",
+	},
+};
